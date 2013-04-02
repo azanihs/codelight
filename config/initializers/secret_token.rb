0f16dfd343446746e07b1f6e595af22375ba8856
muhammad azani <muhammad.azani@gmail.com>
2013-04-02 17:00:51 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codelight::Application.config.secret_token = 'f6adfbcee1278d3034f6bb850a947dbc6560b11652157dd92b96b9a499ca9e0be1d46fbc0e43cbf3fd7a2d2be6669845e931967cc78d2fc5d2cf32ddd23dc118'
